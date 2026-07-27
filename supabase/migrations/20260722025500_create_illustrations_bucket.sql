e5dbe80ff211ad01a37906bdedde604698723368
Kayke-Queiroz <kaykekato@gmail.com>
2026-07-27 08:53:52 -0300

    chore: add UPDATE storage policy and mascot placeholder

--- supabase/migrations/20260722025500_create_illustrations_bucket.sql
+++ supabase/migrations/20260722025500_create_illustrations_bucket.sql
@@ -1,6 +1,7 @@
 -- Drop policies if they already exist
 drop policy if exists "Allow public read access to illustrations" on storage.objects;
 drop policy if exists "Allow public insert access to illustrations" on storage.objects;
+drop policy if exists "Allow public update access to illustrations" on storage.objects;
 
 -- Create storage bucket if it does not exist
 insert into storage.buckets (id, name, public)
@@ -15,3 +16,8 @@ using (bucket_id = 'illustrations');
 create policy "Allow public insert access to illustrations"
 on storage.objects for insert
 with check (bucket_id = 'illustrations');
+
+create policy "Allow public update access to illustrations"
+on storage.objects for update
+using (bucket_id = 'illustrations');
+
